05960248f172b8cee0d5a9bbe5bc4cf10818b53f
zeyad <aziad711@gmail.com>
2026-08-01 13:06:27 +0300

    ✅ NEW MODEL ADDED : NOTE

---
+++ src/model/message.model.js
@@ -0,0 +1,44 @@
+import { Schema } from "mongoose";
+const noData = "NO DATA FOUND !"
+// NOTE SCHEMA 
+const noteSchema = new Schema({
+    // TITLE 
+    title: {
+        type: String,
+        required: true,
+        trim: true,
+        minlength: [3, "TITLE MUST BE AT LEAST 3 CHARACTERS"],
+        validate: {
+            validator: function (value) {
+                return value.toLowerCase();
+            },
+            message: "TITLE CANNOT BE UPPER CASE !"
+        }
+    },
+    // CONTENT 
+    content: {
+        type: String,
+        required: true,
+        trim: true,
+        minlength: [10, "CONTENT MUST BE AT LEAST 10 CHARACTERS"],
+    },
+    // USER ID
+    userId: {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+        required: true,
+    },
+
+},
+    {
+        timestamps: true,
+        collection: "note_data",
+        toJSON: { getters: true, virtuals: true },
+        toObject: { getters: true, virtuals: true },
+        strict: true,
+        strictQuery: true,
+        versionKey: "version",
+    }
+)
+const NoteModel = mongoose.model("Note", noteSchema)
+export default NoteModel
